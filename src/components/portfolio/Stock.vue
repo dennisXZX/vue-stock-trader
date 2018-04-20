89c4797559fb0a83248842457384cde76061bbbd
Dennis Xiao <dennisboys@gmail.com>
2018-04-21 09:42:44 +1000

    added routes

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="container">
-        <router-view></router-view>
+    <div>
+        portfolio - stock component
     </div>
 </template>
 
@@ -10,6 +10,6 @@
   };
 </script>
 
-<style>
+<style scoped>
 
 </style>
