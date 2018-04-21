ab9acb51ace23ee6b834d607199ca7a3ff4f8989
Dennis Xiao <dennisboys@gmail.com>
2018-04-21 11:21:25 +1000

    added stocks components

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,13 +1,30 @@
 <template>
     <div>
-        stocks - stocks component
+        <re-stock
+                v-for="stock in stocks"
+                :stock="stock"
+                :key="stock.id"></re-stock>
     </div>
 </template>
 
 <script>
-  export default {
+  import Stock from './Stock.vue'
 
-  };
+  export default {
+    components: {
+      reStock: Stock
+    },
+    data() {
+      return {
+        stocks: [
+          { id: 1, name: 'BMW', price: 110 },
+          { id: 2, name: 'ASX', price: 210 },
+          { id: 3, name: 'AWS', price: 10 },
+          { id: 4, name: 'Apple', price: 410 },
+        ]
+      }
+    }
+  }
 </script>
 
 <style scoped>
